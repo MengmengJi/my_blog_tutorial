965a4e27619a2134271b18db739417b25b7fae14
jimengmeng <735120042@qq.com>
2018-01-27 15:04:17 +0800

    second

--- article/views.py
+++ article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -9,13 +10,13 @@ def home(request):
     return render(request, 'home.html', {'post_list':post_list})
 
 def detail(request, my_args):
-    post = Article.objects.all()[int(my_args)]
-    str = ("title = %s, category = %s, date_time = %s, content = %s" 
-        % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+    try:
+        post = Article.objects.get(id = str(id))
+        #因为id是每个博文的唯一标识，所以这里使用id对数据库中的博文进行查找
+    except Article.DoesNotExist:
+        raise Http404 
+    return render(request, 'post.html', {'post': post})
     #return HttpResponse("You're looking at my_args %s." % my_args)
 
-def test(request):
-    return render(request, 'test.html', {'current_time': datetime.now()})
     #render函数中第一个参数是request对象， 第二个参数是一个模版名称， 第三个参数是一个字典类型的可选参数
     #它将返回一个包含有给定模版，根据给定的上下文渲染结果的 HttpResponse 对象
